c8fa44543639ac24f03c14797e8205d6b7659dbb
yello <aikyzu25@gmail.com>
2025-04-19 23:47:18 +0800

    updated print

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+    print("Received message:", event.message.text)
 
 
 if __name__ == "__main__":
