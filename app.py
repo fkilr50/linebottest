35d5a9371c1a3161548ebad19ddb7ac34b079f83
yello <aikyzu25@gmail.com>
2025-04-19 22:24:30 +0800

    mini change of strings

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    """ Here's all the messages will be handled and processed by the program """
+    """ Here is all the messages will be handled and processed by the program """
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
