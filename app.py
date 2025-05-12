4e988ba44d89c9dcec924ac2c58c78e1f6f1438d
yello <aikyzu25@gmail.com>
2025-05-12 20:31:20 +0800

    updated requirements

--- app.py
+++ app.py
@@ -255,3 +255,7 @@ def getpass(string):
             else:
                 pword += string[i]
     return pword 
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
